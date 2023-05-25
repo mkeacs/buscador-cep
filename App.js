fe18d3755c479fe6127463e0a510f03a632db9ff
Mike <mkeacs@gmail.com>
2023-05-25 13:25:29 -0300

    add asyncstorage

--- App.js
+++ App.js
@@ -1,151 +1,188 @@
-
 import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { Button, StyleSheet, Text, TextInput, View} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-// componente principal
 export default function App() {
+  // Definição dos estados usando o useState
+  const [cep, setCep] = useState("");
+  const [endereco, setEndereco] = useState(null);
+  const [load, setLoad] = useState(false);
+  const [erro, setErro] = useState();
+  const [enderecoSalvo, setEnderecoSalvo] = useState(null);
+  const [exibirBuscarEndereco, setExibirBuscarEndereco] = useState(true);
 
-  // useState é um hook do React que adiciona estado a um componente funcional.
-  // Aqui temos três estados, o primeiro (cep) armazena o cep digitado pelo usuário.
-  // O segundo (endereco) armazena o objeto de endereço retornado pela API.
-  // O terceiro (load) é um booleano que indica se a aplicação está carregando dados.
-  const [cep,setCep] = useState("")
-  const [endereco, setEndereco] = useState(null)
-  const [load, setLoad] = useState(false)
+  // Função para salvar o endereço no AsyncStorage
+  const salvarEndereco = async () => {
+    try {
+      await AsyncStorage.setItem('endereco', JSON.stringify(endereco));
+      console.log('Endereço salvo com sucesso!');
+    } catch (error) {
+      console.log('Erro ao salvar o endereço:', error);
+    }
+  };
 
-  // O estado erro armazena uma mensagem de erro que pode ser exibida na tela.
-  const [erro, setErro] = useState()
+  // Carrega o endereço salvo ao iniciar o aplicativo
+  useEffect(() => {
+    const carregarEnderecoSalvo = async () => {
+      try {
+        const enderecoSalvo = await AsyncStorage.getItem('endereco');
+        if (enderecoSalvo !== null) {
+          setEnderecoSalvo(JSON.parse(enderecoSalvo));
+        }
+      } catch (error) {
+        console.log('Erro ao carregar o endereço salvo:', error);
+      }
+    };
 
-  // A função buscarCep é chamada quando o botão é pressionado.
+    carregarEnderecoSalvo();
+  }, []);
+
+  // Função para buscar o CEP digitado
   const buscarCep = () => {
-
-    // Validando se o cep é válido.
+    // Verifica se o CEP possui o formato válido
     if(cep.replace("-","",".",",","!","@","'","#","$","%","%","¨","&","*","(",")","-","+","=",">","<",";",":","/","?","´","`","^","~","[","]","{","}","").length != 8){
       alert("Cep Inválido")
       return
     }
 
-    // Se o cep for válido, o estado load é atualizado para true,
-    // indicando que a aplicação está carregando dados.
-    setLoad(true)
+    setLoad(true);
 
-    // Utilizando fetch para fazer uma requisição para a API do ViaCEP.
-    fetch(`https://viacep.com.br/ws/${cep.replace("-","")}/json/`)
+    // Faz uma requisição para a API de CEPs
+    fetch(`https://viacep.com.br/ws/${cep.replace("-", "")}/json/`)
       .then(response => response.json())
       .then(objeto => {
-        if(objeto.erro){
-          // Se a API retornar um objeto com a propriedade erro,
-          // o estado erro é atualizado com uma mensagem de erro.
-          setErro("Cep não encontrado!")
-          return
+        if (objeto.erro) {
+          setErro("Cep não encontrado!");
+          return;
         }
 
-        // Se a API retornar um objeto de endereço válido, 
-        // o estado endereco é atualizado com esse objeto.
-        setEndereco(objeto)
-        setErro("")
+        return salvarEndereco() // Salva o endereço no AsyncStorage
+          .then(() => {
+            setEndereco(objeto);
+            setErro("");
+            setExibirBuscarEndereco(false);
+          })
+          .catch(error => {
+            console.log('Erro ao salvar o endereço:', error);
+          })
+          .finally(() => {
+            setLoad(false);
+          });
       })
       .catch(() => {
-        // Se ocorrer um erro durante a busca pelo endereço,
-        // o estado erro é atualizado com uma mensagem de erro.
-        setErro("Ocorreu um erro ao buscar o endereço!")
-      })
-      .finally(() => {
-        // Após a requisição, o estado load é atualizado para false,
-        // indicando que a aplicação terminou de carregar os dados.
-        setLoad(false)
-      })
-  }
+        setErro("Ocorreu um erro ao buscar o endereço!");
+        setLoad(false);
+      });
+  };
 
-  // Renderização do componente
+  // Função para exibir o endereço salvo
+  const exibirEnderecoSalvo = () => {
+    if (enderecoSalvo) {
+      return (
+        <View style={styles.addressContainer}>
+          <Text style={styles.addressText}>Cep: {enderecoSalvo.cep}</Text>
+          <Text style={styles.addressText}>Rua: {enderecoSalvo.logradouro}</Text>
+          <Text style={styles.addressText}>Complemento: {enderecoSalvo.complemento}</Text>
+          <Text style={styles.addressText}>Bairro: {enderecoSalvo.bairro}</Text>
+          <Text style={styles.addressText}>Cidade: {enderecoSalvo.localidade}</Text>
+          <Text style={styles.addressText}>UF: {enderecoSalvo.uf}</Text>
+          <Text style={styles.addressText}>DDD: {enderecoSalvo.ddd}</Text>
+        </View>
+      );
+    } else {
+      return <Text style={styles.noAddressText}>Nenhum endereço salvo encontrado.</Text>;
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Buscador de endereço</Text>
 
-      {/* Input para o usuário digitar o cep */}
-      <Text style={styles.text}>Digite o CEP</Text>
-      <TextInput placeholder="Digite aqui" style={styles.input}  value={cep} onChangeText={input => setCep(input)}/>
+      {exibirBuscarEndereco && (
+        <>
+          <Text style={styles.text}>Digite o CEP</Text>
+          <TextInput
+            placeholder="Digite aqui"
+            style={styles.input}
+            value={cep}
+            onChangeText={input => setCep(input)}
+          />
+          {load && <Text style={styles.loading}>Carregando...</Text>}
+          {erro && <Text style={styles.error}>{erro}</Text>}
+          <Button style={styles.button} title="Buscar endereço" onPress={buscarCep} />
+        </>
+      )}
 
-      {/* Botão para buscar o endereço */}
-      <Button style={styles.button} title="Buscar endereço" onPress={buscarCep}/>
+      {!exibirBuscarEndereco && (
+        <>
+          <Text style={styles.savedAddressTitle}>Endereço Salvo:</Text>
+          {exibirEnderecoSalvo()}
+          <Button style={styles.button} title="Exibir endereço salvo" onPress={() => setExibirBuscarEndereco(true)} />
+        </>
+      )}
 
-        {/* Renderiza o texto "Carregando..." enquanto a requisição está em andamento */}
-  {load && <Text style={styles.loading}>Carregando...</Text>}
-
-{/* Renderiza os dados do endereço se a requisição foi concluída com sucesso */}
-{endereco != null && !load && erro == "" && (
-  <View style={styles.result}>
-    <Text style={styles.resultText}>Cep: {endereco.cep}</Text>
-    <Text style={styles.resultText}>Rua: {endereco.logradouro}</Text>
-    <Text style={styles.resultText}>Complemento: {endereco.complemento}</Text>
-    <Text style={styles.resultText}>Bairro: {endereco.bairro}</Text>
-    <Text style={styles.resultText}>Cidade: {endereco.localidade}</Text>
-    <Text style={styles.resultText}>UF: {endereco.uf}</Text>
-    <Text style={styles.resultText}>DDD: {endereco.ddd}</Text>
-  </View>
-)}
-
-{/* Renderiza a mensagem de erro caso a requisição falhe */}
-{erro && <Text style={styles.error}>{erro}</Text>}
-
-{/* StatusBar para configurar a barra de status do aplicativo */}
-<StatusBar style="auto" />
-</View>
-);
-
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
-// Estilos
 const styles = StyleSheet.create({
-container: {
-flex: 1,
-backgroundColor: "#fff",
-alignItems: "center",
-justifyContent: "center",
-},
-title: {
-fontSize: 25,
-color: "#264653",
-marginBottom: 20,
-},
-text: {
-fontSize: 18,
-color: "black",
-},
-input: {
-marginVertical: 20,
-height: 40,
-width: 200,
-borderColor: "gray",
-borderWidth: 1,
-paddingHorizontal: 10,
-},
-loading: {
-fontSize: 20,
-marginVertical: 10,
-},
-result: {
-marginTop: 20,
-backgroundColor: "#e5e5e5",
-borderRadius: 5,
-padding: 10,
-width: "90%",
-},
-resultText: {
-fontSize: 16,
-color: "#666",
-marginVertical: 2,
-},
-error: {
-color: "red",
-marginTop: 10,
-fontSize: 18,
-},
-button: {
-marginHorizontal: 20,
-borderRadius: 20,
-color: "red",
-marginBottom: 30,
-},
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 25,
+    color: "#264653",
+    marginBottom: 20,
+  },
+  text: {
+    fontSize: 18,
+    color: "black",
+  },
+  input: {
+    marginVertical: 20,
+    height: 40,
+    width: 200,
+    borderColor: "gray",
+    borderWidth: 1,
+    paddingHorizontal: 10,
+  },
+  loading: {
+    fontSize: 20,
+    marginVertical: 10,
+  },
+  error: {
+    color: "red",
+    marginTop: 10,
+    fontSize: 18,
+  },
+  button: {
+    marginHorizontal: 20,
+    borderRadius: 20,
+    color: "red",
+    marginBottom: 30,
+  },
+  savedAddressTitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  addressContainer: {
+    marginTop: 10,
+  },
+  addressText: {
+    fontSize: 16,
+    color: "#666",
+    marginVertical: 2,
+  },
+  noAddressText: {
+    fontSize: 16,
+    color: "#666",
+    marginVertical: 10,
+    textAlign: "center",
+  },
 });
